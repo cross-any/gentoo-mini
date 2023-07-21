c7299b0d06cc10b8ed84078cb30431af1ce08516
Wind Li <liyong@alot.pw>
2023-07-21 18:47:16 +0800

    [push]Fix repaire

---
+++ repair-aarch64.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+locale-gen
+rm -f /etc/portage/repos.conf/*
+cd /var/db/pkg
+find dev-perl -type d|grep /|sed "s/dev-perl/=dev-perl/g"|xargs emerge -v -j$(nproc)
+rm -rf /var/db/repos/gentoo/*
--- repair-mini.sh
+++ repair-mini.sh
@@ -2,5 +2,3 @@
 set -e
 locale-gen
 rm -f /etc/portage/repos.conf/*
-cd /var/db/pkg
-find dev-perl -type d|grep /|sed "s/dev-perl/=dev-perl/g"|xargs emerge -v -j$(nproc)
