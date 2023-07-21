a06db6d3f1a220249e4e77cc4ddafa16a475d8aa
Wind Li <liyong@alot.pw>
2023-07-22 07:28:25 +0800

    [push]run push arm64 in a new job

--- build-mini.sh
+++ build-mini.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 set -e
 set -x
-if ! which nativerun >/dev/null 2>/dev/null; then
-    alias nativerun=time
+JOBS=$(nproc --all||echo 2)
+if [ "$JOBS" = 1 ]; then
+    JOBS=2
 fi
-nativerun emerge -vn patchelf
+nativerun emerge -vn -j$JOBS patchelf
 ROOT=${ROOT:=/mini}
 sed -i "s/O2/Os -ffunction-sections -fdata-sections/g" /etc/portage/make.conf \
-    && nativerun emerge -vn merge-usr \
+    && nativerun emerge -vn -j$JOBS merge-usr \
     && nativerun merge-usr \
     && nativerun eselect profile set 19 \
     && mkdir -p $ROOT/etc/ \
     && cp -avf /etc/portage $ROOT/etc/ \
     && mkdir -p $ROOT/usr/{lib,lib64,bin} && ln -s usr/bin $ROOT/sbin && ln -s usr/{bin,lib,lib64} $ROOT/ && ln -s bin $ROOT/usr/sbin
 
-PYTHON_TARGETS="python3_11" USE="-multilib -split-usr openmp pam" ROOT=$ROOT emerge -vn -j$(nproc) =sys-libs/glibc-2.36-r8 sys-kernel/linux-headers =sys-devel/gcc-12.2.1_p20230428-r1
+PYTHON_TARGETS="python3_11" USE="-multilib -split-usr openmp pam" ROOT=$ROOT emerge -vn -j$JOBS -vn =sys-libs/glibc-2.36-r8 sys-kernel/linux-headers =sys-devel/gcc-12.2.1_p20230428-r1
 patchelf --remove-rpath $ROOT/lib/ld*so*
 patchelf --remove-rpath $ROOT/lib/libc.so.6
 
-PYTHON_TARGETS="python3_11" USE="-multilib -split-usr pam" ROOT=$ROOT emerge  --autounmask-continue --autounmask=y --autounmask-write -vn -j$(nproc) =sys-devel/gcc-12.2.1_p20230428-r1 coreutils sys-apps/portage dev-vcs/git sys-devel/binutils  \
+PYTHON_TARGETS="python3_11" USE="-multilib -split-usr pam" ROOT=$ROOT emerge  --autounmask-continue --autounmask=y --autounmask-write -vn -j$JOBS -vn =sys-devel/gcc-12.2.1_p20230428-r1 coreutils sys-apps/portage dev-vcs/git sys-devel/binutils  \
     sys-apps/shadow \
     app-arch/gzip \
     sys-process/procps \
